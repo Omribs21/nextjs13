659e8d52a331198abd29d37b0bb7296902a63045
Omribs21 <omri2361@gmail.com>
2023-12-10 22:37:27 +0200

    SideBars

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,7 @@ export default function RootLayout({
             {children}
             </ThemeProvider>
         </ClerkProvider>
+        
       </body>
     </html>
   );
--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -6,24 +6,26 @@ interface ThemeContextType{
     mode: string;
     setMode:(mode:string) => void;
 }
+
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("");
+  const [mode, setMode] = useState('');
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
-      setMode("light");
-
-      document.documentElement.classList.add("light");
-    } else {
+    if (localStorage.theme === 'dark' || !("theme" in localStorage) 
+    && window.matchMedia("(prefers-color-scheme: dark)").matches) {
       setMode("dark");
+
       document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.remove("dark");
     }
   }
 
   useEffect(() => {
-    handleThemeChange()
+    handleThemeChange();
 
   }, [mode])
   
@@ -34,10 +36,12 @@ export function ThemeProvider({ children }: { children: React.ReactNode }) {
   );
 }
 
-export function useTheme () {
+export function useTheme() {
     const context = useContext(ThemeContext);
 
     if (context === undefined) {
         throw new Error('useTheme must be used witin a ThemeProvider');
     }
+
+    return context;
 }
